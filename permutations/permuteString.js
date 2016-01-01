1d43dbbb1e10d27a47f3d4c71358e56693ad81b8
Ignacio Prado <ignacio@gatogordoweb.com>
2016-01-01 17:00:05 -0500

    Double reduce implementation

--- permutations/permuteString.js
+++ permutations/permuteString.js
@@ -3,17 +3,29 @@ Given a string, return all the permutations of that string in alphabetical order
 */
 
 function permutations(str){
-	var perms = [];
-	if(str.length === 1) perms.push(str);
-	str.split('').forEach(function(ch, i, strArr){
-		var others = strArr
-									.filter(function(ch2) { return ch !== ch2; } )
-									.join('');
-		permutations(others).forEach(function(perm){
-			perms.push(ch + perm);
-		});
-	});
-	return perms.sort();
+	if(str.length === 1) return [str];
+	return 	str
+					.split('')
+					.reduce(function(perms, ch, i, strArr){
+						return 	perms
+											.push(permutations(
+												strArr
+													.filter(function(ch2) { 
+														return ch !== ch2; 
+													} )
+											.join('')
+											)
+											.map(function(perm){
+												return ch + perm;
+											})) && perms;
+					}, [])
+					.reduce(function(flattened, twoD){
+						twoD.forEach(function(arry){
+							flattened.push(arry);
+						});
+						return flattened;
+					}, [])
+					.sort();
 }
 
 console.log(permutations("abc"));
