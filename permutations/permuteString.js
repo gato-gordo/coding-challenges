553e6bc4a02e5e4aa034c23afa44041a14a8370f
Ignacio Prado <ignacio@gatogordoweb.com>
2016-01-19 14:37:56 -0600

    Refactor permuations

--- permutations/permuteString.js
+++ permutations/permuteString.js
@@ -3,31 +3,38 @@ Given a string, return all the permutations of that string in alphabetical order
 */
 
 function permutations(str){
-	if(str.length === 1) return [str];
-	return 	str
-					.split('')
-					.reduce(function(perms, ch, i, strArr){
-						return 	perms
-											.push(permutations(
-												strArr
-													.filter(function(ch2) { 
-														return ch !== ch2; 
-													} )
-											.join('')
-											)
-											.map(function(perm){
-												return ch + perm;
-											})) && perms;
-					}, [])
-					.reduce(function(flattened, twoD){
-						twoD.forEach(function(arry){
-							flattened.push(arry);
-						});
-						return flattened;
-					}, [])
-					.sort();
+  if(str.length === 1) return [str];
+  return  str
+    .split('')
+    .reduce(function(perms, chValOuter, chIndOuter, chArr){
+    	return 	perms.concat(
+                permutations(
+                	chArr
+                	  .filter(function(chValInner, chIndInner){ 
+                	  	return chIndOuter !== chIndInner; 
+                    } )
+                    .join('')
+                )
+                .map(function(innerPerm)
+                	return chValOuter + innerPerm;
+                })
+							);
+    }, [])
+    .sort();
 }
 
-console.log(permutations("abc"));
+function permutations(str){
+	if(str.length === 1) return [str];
+	var outerPerms = [];
+	for(var i = 0; i &lt; str.length; i++){
+		var innerPerms = permutations(str.slice(0, i) + str.slice(i + 1));
+		for(var j = 0; j &lt; innerPerms.length; j++){
+			outerPerms.push(str[i] + innerPerms[j]);
+		}
+	}
+	return outerPerms.sort();
+}
+
+console.log(permutations("aeef"));
 
 
