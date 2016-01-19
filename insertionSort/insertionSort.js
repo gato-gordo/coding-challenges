6bd365c6e748cb1bfaeb38d60dae08976212d73e
Ignacio Prado <ignacio@gatogordoweb.com>
2016-01-19 14:37:10 -0600

    Add specification to insertionSort

--- insertionSort/insertionSort.js
+++ insertionSort/insertionSort.js
@@ -1,3 +1,8 @@
+/*
+Insertion sort is a basic sorting algorithm. Insertion sort iterates over an array, growing a sorted array behind the current location. It takes each element from the input and finds the spot, up to the current point, where that element belongs. It does this until it gets to the end of the array.
+
+*/
+
 function insertionSort (arr) {
   var sortUpTo  = 0
   while(++sortUpTo < arr.length){
